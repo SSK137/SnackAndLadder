1acd0921516125410706b28663c0e2fda100b814
SSK137 <saurabhkodam@gmail.com>
2022-04-25 13:52:34 +0530

    SnackandLadder : Playes Options

--- src/Snack_Ladder_Problem.java
+++ src/Snack_Ladder_Problem.java
@@ -1,6 +1,9 @@
 import java.util.*;
 public class Snack_Ladder_Problem {
     public static final int START_POSITION_LADDER = 0;
+    public static final int NO_FUTHER_PLAY = 0;
+    public static final int LADDER = 1;
+    public static final int SNAKE = 2;
     public static void main(String[] args) {
         System.out.println("Welcome To Snake And Ladder Game");
         System.out.println("Roll The Dies");
@@ -9,5 +12,19 @@ public class Snack_Ladder_Problem {
         int dies = random.nextInt(6) + 1;
         System.out.println("Dies Value Is " + dies);
 
+        int options = random.nextInt(3);
+        System.out.println(options);
+        switch (options){
+            case LADDER :
+                position += dies;
+                break;
+            case SNAKE:
+                if (position != 0)
+                    position -= dies;
+                break;
+            case NO_FUTHER_PLAY:
+                break;
+        }
+        System.out.println("Player At Position " + position);
     }
 }
