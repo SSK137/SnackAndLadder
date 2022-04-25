10c5eaa8b7f60c3e1da8a26723135d1455ddf2ea
SSK137 <saurabhkodam@gmail.com>
2022-04-25 13:48:20 +0530

    SnackandLadder : Playes Dies

--- src/Snack_Ladder_Problem.java
+++ src/Snack_Ladder_Problem.java
@@ -1,7 +1,13 @@
+import java.util.*;
 public class Snack_Ladder_Problem {
-    public static final int START_POSITION_LADDER = 0;           // possition initialized
+    public static final int START_POSITION_LADDER = 0;
     public static void main(String[] args) {
-        System.out.println("Welcome To Snake And Ladder");
+        System.out.println("Welcome To Snake And Ladder Game");
+        System.out.println("Roll The Dies");
+        Random random = new Random();
         int position= 0;
+        int dies = random.nextInt(6) + 1;
+        System.out.println("Dies Value Is " + dies);
+
     }
 }
