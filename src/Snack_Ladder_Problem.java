6b0dcbd5181e8f4540e627d5165da60d485ff1c4
SSK137 <saurabhkodam@gmail.com>
2022-04-25 13:36:23 +0530

    SnackandLadder : Start Position

--- src/Snack_Ladder_Problem.java
+++ src/Snack_Ladder_Problem.java
@@ -1,5 +1,7 @@
 public class Snack_Ladder_Problem {
+    public static final int START_POSITION_LADDER = 0;           // possition initialized
     public static void main(String[] args) {
-        System.out.println("Welcomr To Snack And Ladder Problem");
+        System.out.println("Welcome To Snake And Ladder");
+        int position= 0;
     }
 }
